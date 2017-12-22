1107e8eb3cd85b739bba3781c8f3c5a793bd6fb7
kagikn <kagikn.3939@gmail.com>
2017-12-22 18:21:15 +0900

    Use GetPickupObjectHandles instead of GetPickupObjectAddresses

--- GlowingPickups/GlowingPickups.cs
+++ GlowingPickups/GlowingPickups.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
@@ -38,53 +38,9 @@ namespace GlowingPickups
 
             var offset = (int)Game.Version >= (int)GameVersion.VER_1_0_944_2_STEAM ? 0x480 : 0x470;
 
-            foreach (var pickupAddr in PickupObjectPoolTask.GetPickupObjectAddresses())
+            var pickupProps = PickupObjectPoolTask.GetPickupObjectHandles();
+            foreach (var pickup in pickupProps)
             {
-                unsafe
-                {
-                    var isVisible = (Marshal.ReadByte(pickupAddr, 0x2C) & 0x01) == 1;
-
-                    if (!isVisible)
-                    {
-                        continue;
-                    }
-
-                    var pos = *(Vector3*)(pickupAddr + 0x90);
-                    var dataAddress = Marshal.ReadIntPtr(pickupAddr, offset);
-
-                    if (dataAddress != IntPtr.Zero)
-                    {
-                        var red = (int)(BitConverter.ToSingle(
-                            BitConverter.GetBytes(Marshal.ReadInt32(dataAddress, 0x5C)), 0) * 255);
-                        var green = (int)(BitConverter.ToSingle(
-                            BitConverter.GetBytes(Marshal.ReadInt32(dataAddress, 0x60)), 0) * 255);
-                        var blue = (int)(BitConverter.ToSingle(
-                            BitConverter.GetBytes(Marshal.ReadInt32(dataAddress, 0x64)), 0) * 255);
-                        var range = BitConverter.ToSingle(
-                            BitConverter.GetBytes(Marshal.ReadInt32(dataAddress, 0x10)), 0) * settings.RangeMultiplier;
-                        var intensity = BitConverter.ToSingle(
-                            BitConverter.GetBytes(Marshal.ReadInt32(dataAddress, 0x68)), 0) * settings.LightIntensityMultiplier;
-                        var darkIntensity = BitConverter.ToSingle(
-                            BitConverter.GetBytes(Marshal.ReadInt32(dataAddress, 0x6C)), 0) * settings.ShadowMultiplier;
-                        Function.Call(Hash._DRAW_LIGHT_WITH_RANGE_WITH_SHADOW, pos.X, pos.Y, pos.Z, red,
-                        green, blue, range, intensity, darkIntensity);
-                    }
-                    else
-                    {
-                        Function.Call(Hash._DRAW_LIGHT_WITH_RANGE_WITH_SHADOW, pos.X, pos.Y, pos.Z, 255, 57, 0, 5.0f, 30.0f, 10.0f);
-                    }
-                }
-            }
-
-            // AddEntityToPoolFunc in GetPickupObjectHandles() isn't working
-
-            /*  var pickupProps = PickupObjectPoolTask.GetPickupObjectHandles();
-                foreach (var pickup in pickupProps)
-            {
-                //0x470 - in biker update
-
-                var offset = (int)Game.Version >= (int)GameVersion.VER_1_0_944_2_STEAM ? 0x480 : 0x470;
-
                 unsafe
                 {
                     var dataAddress = Marshal.ReadIntPtr(new IntPtr(pickup.MemoryAddress), offset);
@@ -114,7 +70,7 @@ namespace GlowingPickups
                         Function.Call(Hash._DRAW_LIGHT_WITH_RANGE_WITH_SHADOW, pos.X, pos.Y, pos.Z, 255, 57, 0, 5.0f, 30.0f, 10.0f);
                     }
                 }
-            }*/
+            }
         }
     }
 }
